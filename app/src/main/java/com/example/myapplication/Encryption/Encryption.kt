d54dc624e879327c88f9f4970165ba83c10191dd
Supratim2000 <pikucode15@gmail.com>
2022-07-01 22:09:46 +0530

    Creating aesDecryption method inside Encryption class as companion object

--- app/src/main/java/com/example/myapplication/Encryption/Encryption.kt
+++ app/src/main/java/com/example/myapplication/Encryption/Encryption.kt
@@ -19,5 +19,18 @@ class Encryption {
             }
             return encryptedText
         }
+
+        fun aesDecryption(text: String): String {
+            var decryptedText: String ="-1"
+            if(text != "-1") {
+                try {
+                    decryptedText = AESCrypt.decrypt(ConstantValues.AES_ENCRIPTION_KEY_FOR_USER_PASSWORD, text)
+                } catch(e: GeneralSecurityException) {
+                    Log.v(ConstantValues.LOGCAT_TEST, "AES Encryption failed")
+                    e.printStackTrace()
+                }
+            }
+            return decryptedText
+        }
     }
 }
